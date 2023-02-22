c7501bda1dabe8c2b6e80d0c9694da6c0bf35bbb
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-22 12:33:34 -0600

    Unit 4 final project initial upload, step 4 complete

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,23 @@
 class Game {
     constructor() {
         this.missed = 0,
-        this.phrases = [],
+        this.phrases = [
+            {
+                "phrase": "Life is like a box of chocolates"
+            },
+            {
+                "phrase": "There is no I in team"
+            },
+            {
+                "phrase": "The Matrix has you"
+            },
+            {
+                "phrase": "May the Force be with you"
+            },
+            {
+                "phrase": "May the Force be with you have to see the Matrix for yourself"
+            }
+        ],
         this.activePhrase = null
     }
 }
--- js/app.js
+++ js/app.js
@@ -7,3 +7,9 @@
 
 // const phrase = new Phrase('Life is like a box of chocolates');
 // console.log(`Phrase - phrase: ${phrase.phrase}`);
+
+// const game = new Game();
+//
+// game.phrases.forEach((phrase, index) => {
+//     console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+// });
