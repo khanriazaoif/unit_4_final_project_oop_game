da700349615e6d787198a90b017f4108e706258d
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-03-19 14:09:34 -0500

    Unit 4 final project initial upload, working on step 7 complete

--- js/Game.js
+++ js/Game.js
@@ -6,21 +6,11 @@ class Game {
     constructor() {
         this.missed = 0,
         this.phrases = [
-            {
-                "phrase": "Life is like a box of chocolates"
-            },
-            {
-                "phrase": "There is no I in team"
-            },
-            {
-                "phrase": "The Matrix has you"
-            },
-            {
-                "phrase": "May the Force be with you"
-            },
-            {
-                "phrase": "You have to see the Matrix for yourself"
-            }
+            new Phrase("Life is like a box of chocolates"),
+            new Phrase("There is no I in team"),
+            new Phrase("The Matrix has you"),
+            new Phrase("May the Force be with you"),
+            new Phrase("You have to see the Matrix for yourself")
         ],
         this.activePhrase = null
 
@@ -38,10 +28,9 @@ class Game {
     startGame(){
         const overlay = document.getElementById('overlay');
         overlay.style.display = 'none';
-        let getRandomPhrase = this.getRandomPhrase();
-        console.log(getRandomPhrase);
-        console.log(phrase.addPhraseToDisplay());
-        this.activePhrase = phrase.addPhraseToDisplay;
+        this.activePhrase = this.getRandomPhrase();
+        console.log(this.activePhrase);
+        this.activePhrase.addPhraseToDisplay();
     }
 
 }
